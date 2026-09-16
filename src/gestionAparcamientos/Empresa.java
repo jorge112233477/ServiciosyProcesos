d87485512ed076c7011bdb12f74d8c433cadeca4
Jorge Perez <jorgeimon100@gmail.com>
2026-09-16 12:28:47 +0200

    actualizado

--- src/gestionAparcamientos/Empresa.java
+++ src/gestionAparcamientos/Empresa.java
@@ -1,37 +1,52 @@
 package gestionAparcamientos;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Empresa {
 
     private String nombre;
-    private ArrayList<aparcamientos> aparcamientos;
+
+    private HashMap<String, aparcamientos> aparcamientos;
 
     public Empresa(String nombre) {
+
         this.nombre = nombre;
-        aparcamientos = new ArrayList<>();
+
+        aparcamientos = new HashMap<>();
+
     }
 
     public void registrarAparcamiento(aparcamientos a) {
-        aparcamientos.add(a);
+
+        aparcamientos.put(a.getNombre(), a);
+
     }
 
     public void buscarCiudad(String ciudad) {
 
-        for (aparcamientos a : aparcamientos) {
+        for (aparcamientos a : aparcamientos.values()) {
+
             if (a.getCiudad().equalsIgnoreCase(ciudad)) {
+
                 System.out.println(a.getNombre());
+
             }
+
         }
+
     }
 
     public void mostrarAparcamientos() {
 
-        for (aparcamientos a : aparcamientos) {
+        for (aparcamientos a : aparcamientos.values()) {
+
             System.out.println(
                 a.getNombre() + " - Coches: " +
                 a.getCochesAparcados()
             );
+
         }
+
     }
+
 }
---
+++ src/gestionAparcamientos/ParkingJob.java
@@ -0,0 +1,23 @@
+package gestionAparcamientos;
+
+public class ParkingJob extends Thread {
+	
+	
+	private aparcamientos parking;
+	
+	
+	public ParkingJob(aparcamientos parking1) {
+		Object parking;
+		this.parking = parking1;
+	}
+
+	@Override
+	public void run() {
+		
+		
+		
+	}
+	
+	
+
+}
